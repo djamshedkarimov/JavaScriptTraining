90abf3b7c4486b7646ef1180c32c82022dd0735f
DJ-VAPI <djkarimov@fiveanddone.com>
2020-11-21 22:39:52 -0800

    Training 1st push

---
+++ app.js
@@ -0,0 +1,30 @@
+var count = 100 
+
+var userNameMyNameBegi = 'Begi' + "djhsdsh"; String (text)
+var user_name_my_name = "Begi";
+
+var ten = '10';
+'My Name is "Begi". I am ' + 
+var combined = age + ' old'; 
+
+
+var result = ten + count;
+
+var begi = userNameMyNameBegi + user_name_my_name + result;
+
+combined = "Hey Your name is not Begi!"
+
+10 / 2 = 5
+10 % 2 = 0
+15 % 2 = 1      15/4=3.75
+
+var result2 = 15 % 4; 
+
+javascript function
+
+combined = "THIS IS NOT TRUE"
+
+var calculation = 1 + (3 * 3) - (1 / 2);
+
+console.log(result2);
+
